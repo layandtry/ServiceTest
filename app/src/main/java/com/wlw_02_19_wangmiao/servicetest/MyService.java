c54d5e17272111e7fc79314d4151e6c53bd8eb84
layandtry <lay_and_try@163.com>
2018-06-01 15:01:31 +0800

    前台服务，会一直有一个正在运行的图标在系统的状态栏显示

--- app/src/main/java/com/wlw_02_19_wangmiao/servicetest/MyService.java
+++ app/src/main/java/com/wlw_02_19_wangmiao/servicetest/MyService.java
@@ -1,9 +1,13 @@
 package com.wlw_02_19_wangmiao.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class MyService extends Service {
@@ -35,6 +39,18 @@ public class MyService extends Service {
     public void onCreate(){
         super.onCreate();
         Log.d("MyService","onCreate executed");
+        Intent intent = new Intent(this,MainActivity.class);
+        PendingIntent pi;
+        pi = PendingIntent.getActivity(this,0,intent,0);
+        Notification notification = new NotificationCompat.Builder(this)
+                .setContentTitle("This is content title")
+                .setContentText("This is content Text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1,notification);
     }
 
     @Override
