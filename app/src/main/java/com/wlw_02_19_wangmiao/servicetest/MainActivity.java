cda9ece6434880e262b0f230cff3005d223e5291
layandtry <lay_and_try@163.com>
2018-06-01 15:20:21 +0800

    IntentService

--- app/src/main/java/com/wlw_02_19_wangmiao/servicetest/MainActivity.java
+++ app/src/main/java/com/wlw_02_19_wangmiao/servicetest/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -40,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button unbindService = (Button)findViewById(R.id.unbind_service);
         bindService.setOnClickListener(this);
         unbindService.setOnClickListener(this);
+
+        Button startIntentService = (Button)findViewById(R.id.start_service);
+        startIntentService.setOnClickListener(this);
     }
 
     @Override
@@ -59,6 +63,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.unbind_service:
                 unbindService(connection);//解绑服务
+                break;
+            case R.id.start_intent_service:
+                //打印主线程的id
+                Log.d("MainActivity","Thread id is" + Thread.currentThread().getId());
+                Intent intentService = new Intent(this,MyIntentService.class);
+                startService(intentService);
                 break;
             default:
                 break;
--- app/src/main/java/com/wlw_02_19_wangmiao/servicetest/MyService.java
+++ app/src/main/java/com/wlw_02_19_wangmiao/servicetest/MyService.java
@@ -55,7 +55,13 @@ public class MyService extends Service {
 
     @Override
     public int onStartCommand(Intent intent,int flags,int startId){
-        Log.d("MyService","onStartCommand executed");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+            }
+        }).start();
+//        Log.d("MyService","onStartCommand executed");
         return super.onStartCommand(intent,flags,startId);
     }
 
